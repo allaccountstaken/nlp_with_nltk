6b2b451d413400ed8218c2b8e8fc5cb02a299ebf
Dmitry Mikhaylov <dmik@vt.edu>
2020-11-22 11:43:00 -0500

    Tracking down to the synonyms with WorldNet Lemmatizer

--- nlp_functions.py
+++ nlp_functions.py
@@ -1,19 +1,24 @@
+def run_imports():
+    import re
+    import string
+    import nltk
+    import pandas as pd
 
 
 def clean_text(text):
-    """Cleans text string removing punctuation, spliting to tokens, and removing stopwords
 
-    Args:
-        text ([str]): [String, ex sentences in panda columns]
+    import re
+    import string.punctuation
+    import nltk
+    from nltk.corpus import stopwords
+    import pandas as pd
 
-    Returns:
-        [list]: [List of cleaned strings separated by comma]
-    """
+    stopwords = nltk.corpus.stopwords.words('english')
 
     text = "".join([char for char in text if char not in string.punctuation])
     tokens = re.split('\W+', text)
-    text = [word for word in tokens if word not in stopwords]
-    return text
+    tokenized_text = [word for word in tokens if word not in stopwords]
+    return tokenized_text
 
 
 def stemming(tokenized_text):
@@ -25,5 +30,25 @@ def stemming(tokenized_text):
     Returns:
         [type]: [description]
     """
-    text = [ps.stem(word) for word in tokenized_text]
-    return text
+
+    import re
+    import string
+    import nltk
+    import pandas as pd
+
+    ps = nltk.PorterStemmer()
+
+    stemmed_text = [ps.stem(word) for word in tokenized_text]
+    return stemmed_text
+
+
+def lemmatizing(tokenized_text):
+
+    import re
+    import string
+    import nltk
+    import pandas as pd
+    wn = nltk.WordNetLemmatizer()
+
+    lemmatized_text = [wn.lemmatize(word) for word in tokenized_text]
+    return lemmatized_text
