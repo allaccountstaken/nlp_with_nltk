ffc2da830db0e3ebc204917967ab023eb67d446c
Dmitry Mikhaylov <dmik@vt.edu>
2020-11-22 11:06:58 -0500

    function to clean text, tokenize, drop stopwords, stem

--- nlp_functions.py
+++ nlp_functions.py
@@ -1,3 +1,5 @@
+
+
 def clean_text(text):
     """Cleans text string removing punctuation, spliting to tokens, and removing stopwords
 
@@ -11,4 +13,17 @@ def clean_text(text):
     text = "".join([char for char in text if char not in string.punctuation])
     tokens = re.split('\W+', text)
     text = [word for word in tokens if word not in stopwords]
+    return text
+
+
+def stemming(tokenized_text):
+    """[summary]
+
+    Args:
+        tokenized_text ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    text = [ps.stem(word) for word in tokenized_text]
     return text
